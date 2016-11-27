195b561dea257bbcc2988277a50061fdf1a61a47
quinnjn <quinn@neumiiller.com>
2016-11-26 17:14:48 -0800

    Release 0.1.0

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
 const snoowrap = require('snoowrap');
+const url = require('url');
 const app = express();
 const reddit = new snoowrap({
   userAgent: 'redditube',
@@ -9,22 +11,53 @@ const reddit = new snoowrap({
 });
 
 
-app.get('/r/:subreddit', function (req, res) {
-
-  reddit.getSubreddit(req.params.subreddit)
+app.use(morgan('combined'));
+app.get('/r/:subreddit/:videoId*?', function (req, res) {
+  var subreddit = req.params.subreddit
+  var videoId = req.params.videoId || '';
+  reddit.getSubreddit(subreddit)
     .getHot()
     .filter(function (submission) {
-      return /youtube.com/.test(submission.url);
+      return /youtube.com/.test(submission.url) && /v=/.test(submission.url);
     })
-    .map(post => post.url.split('v=')[1])
+    .map(function (post) {
+      console.log(post.url);
+      var videoId = post.url.split('v=')[1]
+        .split('&')[0]
+        .split('?')[0];
+      return {
+       title: post.title,
+       redditUrl: 'http://reddit.com' + post.permalink,
+       videoId: videoId
+      };
+    })
     .then(function (videos, reject) {
-      res.render('index', {
+      console.log(videos);
+      var pointer = videos.findIndex(function (video) {
+        return videoId === video.videoId;
+      });
+      if (pointer === -1) {
+        pointer = 0;
+      } 
+      var videoData = {
+        pointer: pointer,
         videos: videos
+      };
+      console.log({
+        subreddit: subreddit,
+        videoData: {
+          pointer: videoData.pointer,
+          length: videoData.videos.length
+        },
+        url: req.url
+      });
+      res.render('index', {
+        videoData: videoData
       }); 
     });
 });
 
 app.set('view engine', 'pug');
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+ console.log('Example app listening on port 3000!');
 })
